b6629209bb85de89dbb668c2269048504d57a9fe
alunos <alunos@16107L>
2020-02-21 21:50:43 -0300

    Inicio do Projeto Time

---
+++ ProjetoTime/ProjetoTime/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Instancia de um objeto*/
+            Time objTime = new Time();
+            objTime.nome = "Boa Esporte";
+            objTime.cidade = "Varginha";
+            objTime.comprarJogador(1);
+            objTime.titulo(1);
+            objTime.valor(0);
+
+            /*Mostrar Valor para o Usuario*/
+            System.Console.WriteLine(@"Entre com a cidade do time "+ objTime.nome);
+            
+
+            /*Ler entrada do usuário*/
+            objTime.cidade = System.Console.ReadLine();
+
+            /*Mostrar Valor para o Usuario*/
+            System.Console.WriteLine(@"Entre com o estado do time " + objTime.nome);
+
+            /*Ler entrada do usuário*/
+            objTime.estado = System.Console.ReadLine();
+
+            /*Mostrar Valor para o Usuario*/
+            System.Console.WriteLine(@"Entre com o pais do time " + objTime.nome);
+
+            /*Ler entrada do usuário*/
+            objTime.pais = System.Console.ReadLine();
+
+            /*Mostrar Valor para o Usuario*/
+            System.Console.WriteLine(@"Entre com o número de jogadores do " + objTime.nome);
+
+            /*Ler entrada do usuário*/
+            objTime.numerodejogadores = Convert.ToInt32(System.Console.ReadLine());
+
+            /*Mostrar Valor para o Usuario*/
+            System.Console.WriteLine(@"Entre com o número de titulos do " + objTime.nome);
+
+            /*Ler entrada do usuário*/
+            objTime.numerodetitulos = Convert.ToInt32(System.Console.ReadLine());
+
+            /*Mostrar Valor para o Usuario*/
+            System.Console.WriteLine(@"Entre com o Valor do Time " + objTime.nome);
+
+            /*Ler entrada do usuário*/
+            objTime.valorDoTimeEmDinheiro = Convert.ToInt32(System.Console.ReadLine());
+
+            /*Mostrar estado do objeto*/
+            string statusDoObjeto;
+            statusDoObjeto = objTime.statusClass();
+            System.Console.WriteLine(statusDoObjeto);
+            
+            //System.Console.WriteLine(@"Entre com o número de Jogadores");
+            //int numJogador;
+            //numjogador = System.Console.ReadLine();
+            //objTime.comprarJogador(numJogador);
+
+            /*Parar o programa*/
+            System.Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ProjetoTime/ProjetoTime/Time.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTime
+{
+    class Time
+    {
+        /*Atributos (Caracteristicas da Classe)*/
+        public string nome;
+        public string pais;
+        public string estado;
+        public string cidade;
+        public int numerodejogadores = 0;
+        public int numerodetitulos = 0;
+        public double valorDoTimeEmDinheiro = 0;
+
+
+        /*Metodos (Ações)*/
+        public void comprarJogador (int numJogador)
+        {
+            this.numerodejogadores += numJogador;
+        }
+
+        public void titulo(int ganhoutitulo)
+        {
+
+            this.numerodetitulos += ganhoutitulo;
+        }
+
+        public void valor (int entradadedinheiro)
+        {
+
+            this.valorDoTimeEmDinheiro += entradadedinheiro;
+        }
+
+        public string statusClass()
+        {
+            string retorno;
+            retorno = "Nome: " + this.nome +
+                    "\nCidade: " + this.cidade +
+                    "\nEstado: " + this.estado +
+                    "\nPaís: " + this.pais +
+                    "\nNúmero de Jogadores: " + this.numerodejogadores +
+                    "\nNúmero de Titulo: " + this.numerodetitulos +
+                    "\nValor do Time: R$" + this.valorDoTimeEmDinheiro;
+
+
+            return retorno;           
+         }
+
+
+    }
+}
